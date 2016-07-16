6ba355eb3f14f85ad38ab09934bf3053fd484412
Richard Pawson <rpawson@nakedobjects.net>
2016-07-16 14:53:34 +0100

    Story 2

--- Reversi/FromScratch/app.js
+++ Reversi/FromScratch/app.js
@@ -1,1 +1,13 @@
+var canvas;
+var renderer;
+window.onload = function () {
+    canvas = document.getElementsByTagName("canvas")[0];
+    renderer = canvas.getContext("2d");
+    //Draw background
+    renderer.fillStyle = 'green';
+    renderer.fillRect(0, 0, 480, 480);
+    //Draw outline
+    renderer.strokeStyle = 'black';
+    renderer.strokeRect(0, 0, 480, 480);
+};
 //# sourceMappingURL=app.js.map
--- Reversi/FromScratch/app.ts
+++ Reversi/FromScratch/app.ts
@@ -1,1 +1,14 @@
-﻿
+﻿var canvas: HTMLCanvasElement;
+var renderer: CanvasRenderingContext2D;
+
+window.onload = function () {
+    canvas = document.getElementsByTagName("canvas")[0];
+    renderer = canvas.getContext("2d");
+
+    //Draw background
+    renderer.fillStyle = 'green';
+    renderer.fillRect(0, 0, 480, 480);
+    //Draw outline
+    renderer.strokeStyle = 'black';
+    renderer.strokeRect(0, 0, 480, 480);
+}
