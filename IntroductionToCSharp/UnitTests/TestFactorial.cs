cf30ab61fea3ef4833b8b0e0e0ffe34b978658f7
Richard Pawson <rpawson@nakedobjects.net>
2016-11-14 14:15:48 +0000

    Improvements

--- IntroductionToCSharp/UnitTests/TestFactorial.cs
+++ IntroductionToCSharp/UnitTests/TestFactorial.cs
@@ -8,23 +8,23 @@ namespace Test
     public class TestFactorial
     {
         [TestMethod]
-        public void Test1()
+        public void Test_1()
         {
             Assert.AreEqual(1, Program.Factorial(1));
         }
 
         [TestMethod]
-        public void Test2()
+        public void Test_2()
         {
             Assert.AreEqual(2, Program.Factorial(2));
         }
         [TestMethod]
-        public void Test3()
+        public void Test_3()
         {
             Assert.AreEqual(6, Program.Factorial(3));
         }
         [TestMethod]
-        public void Test4()
+        public void Test_4()
         {
             Assert.AreEqual(24, Program.Factorial(4));
         }
@@ -32,6 +32,11 @@ namespace Test
         public void Test10()
         {
             Assert.AreEqual(3628800, Program.Factorial(10));
+        }
+        [TestMethod]
+        public void Test_0()
+        {
+            Assert.AreEqual(1, Program.Factorial(0));
         }
     }
 }
---
+++ Recursion/realisticTree.ts
@@ -0,0 +1,47 @@
+﻿let p = new Pen("mycanvas");
+p.jump(250, 400);
+fork(300, 0.2, 0.4, 10, 40, 10, p);
+p.draw();
+
+function fork(size: number,
+                minTrunk: number,
+                maxTrunk: number,
+                minAngle: number,
+                maxAngle: number,
+                minTwigLength: number,
+                p: Pen) {
+
+    if (size > minTwigLength) {
+        p.penstyle('brown');
+        const trunk = random(size * minTrunk, size * maxTrunk);
+        const width = Math.floor(trunk / 10) + 1;
+        p.pensize(width);
+        const shoot = size - trunk;
+        p.go(trunk);
+        p.draw();
+        let angle = random(minAngle, maxAngle);
+        p.turn(angle);
+        fork(shoot, minTrunk, maxTrunk, minAngle, maxAngle, minTwigLength, p);
+        p.turn(-angle);
+        angle = random(minAngle, maxAngle);
+        p.turn(-angle);
+        fork(shoot, minTrunk, maxTrunk, minAngle, maxAngle, minTwigLength, p);
+        p.turn(angle);
+        p.back(trunk);
+    }
+    else {
+        p.penstyle('green')
+        p.pensize(5);
+        p.go(5);
+        p.back(5);
+        p.draw();
+        p.penstyle('blue');
+        p.penstyle('brown');
+    }
+}
+
+function random(min: number, max: number): number {
+    const range = max - min;
+    return Math.floor(min) + Math.floor(Math.random() * range);
+}
+
