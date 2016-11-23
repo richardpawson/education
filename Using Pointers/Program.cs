e59ba7f071a707482dd257f8073b339a7a52ea7b
Richard Pawson <rpawson@nakedobjects.net>
2016-11-23 15:47:32 +0000

    Added Using Pointers project

---
+++ Using Pointers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingPointers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe {
+                int x = 100;
+
+                int* ptrX = &x; //sets pointer to memory location where x is held
+                Console.WriteLine((int)ptrX); // Displays the memory address
+                Console.WriteLine(*ptrX);// Displays the value at the memory address. 
+
+                int y = 200;
+                int* ptrY = &y;
+                Console.WriteLine((int) ptrY); 
+                Console.WriteLine(*ptrY);  
+            }
+            Console.ReadKey();
+        }
+    }
+}
