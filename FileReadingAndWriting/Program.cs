3cf13f94097828b95ef1e1cb8539a13f21f99b15
Richard Pawson <rpawson@nakedobjects.net>
2017-01-04 16:41:50 +0000

    Improved FileReadingAndWriting -  to use 'using' statements

--- FileReadingAndWriting/Program.cs
+++ FileReadingAndWriting/Program.cs
@@ -7,41 +7,41 @@ namespace FileReadingAndWriting
     {
         static void Main()
         {
-            const string path = @"C:\education\FileReadingAndWriting\";
-            decimal[] columnTotals = null;
-            decimal grandTotal = 0;
-            int columnCount = 0;
-            StreamReader reader = new StreamReader(path+"inputFile.txt");
-            StreamWriter writer = new StreamWriter(path+"outputFile.txt");
-            int row = 0;
-            while (!reader.EndOfStream)
+            using (StreamWriter writer = new StreamWriter("outputFile.txt"))
             {
-                string inputLine = reader.ReadLine();
-                string[] values = inputLine.Split(',');
-                if (columnCount == 0) //i.e. only  on first line
+                decimal[] columnTotals = null;
+                decimal grandTotal = 0;
+                int columnCount = 0;
+                using (StreamReader reader = new StreamReader("inputFile.txt"))
                 {
-                    columnCount = values.Length;
-                    columnTotals = new Decimal[columnCount+ 1]; //extra 1 for grand total
+                    while (!reader.EndOfStream)
+                    {
+                        string inputLine = reader.ReadLine();
+                        string[] values = inputLine.Split(',');
+                        if (columnCount == 0) //i.e. only  on first line
+                        {
+                            columnCount = values.Length;
+                            columnTotals = new Decimal[columnCount + 1]; //extra 1 for grand total
+                        }
+                        decimal lineTotal = 0;
+                        for (int col = 0; col < values.Length; col++)
+                        {
+                            decimal value = Convert.ToDecimal(values[col]);
+                            lineTotal += value;
+                            columnTotals[col] += value;
+                        }
+                        string outputLine = inputLine + "," + lineTotal;
+                        writer.WriteLine(outputLine);
+                        grandTotal += lineTotal;
+                    }
                 }
-                decimal lineTotal = 0;
-                for (int col = 0; col < values.Length; col++)
+                for (int i = 0; i < columnCount; i++)
                 {
-                    decimal value = Convert.ToDecimal(values[col]);
-                    lineTotal += value;
-                    columnTotals[col] += value;
+                    writer.Write(columnTotals[i] + ",");
                 }
-                string outputLine = inputLine + "," + lineTotal;
-                writer.WriteLine(outputLine);
-                grandTotal += lineTotal;
-                row++;
+                writer.WriteLine(grandTotal);
+                writer.Flush();
             }
-            for (int i = 0; i < columnCount; i++)
-            {
-                writer.Write(columnTotals[i] + ",");
-            }
-            writer.WriteLine(grandTotal);
-            writer.Flush();
-
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
