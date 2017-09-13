0aaf434ed91d86de7d7e3aa1902e723316d10767
Richard Pawson <rpawson@nakedobjects.net>
2017-09-13 17:12:21 +0100

    Infinite Sets project added

---
+++ Infinite Sets/FiniteSets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var set = NaturalNumbers();
+            //var set = EvenMembersOf(NaturalNumbers());
+            //var set = SquaresOf(NaturalNumbers());
+            foreach (var n in set) 
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> NaturalNumbers()
+        {
+            return new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+
+        static IEnumerable<int> EvenMembersOf(IEnumerable<int> inputs)
+        {
+            var evens = new List<int>();
+            foreach (var n in inputs)
+            {
+                if (n % 2 == 0) evens.Add(n);
+            }
+            return evens;
+        }
+
+        static IEnumerable<int> SquaresOf(IEnumerable<int> inputs)
+        {
+            var squares = new List<int>();
+            foreach (var n in inputs)
+            {
+                squares.Add(n*n);
+            }
+            return squares;
+        }
+
+    }
+}
---
+++ Infinite Sets/InfiniteSets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //NOTE: long has been used in place of int, to give greater range
+
+        static void Main()
+        {
+            //var set = NaturalNumbers();
+            //var set = EvenMembersOf(NaturalNumbers());
+            var set = SquaresOf(NaturalNumbers());
+            foreach (var n in set)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+
+        static IEnumerable<long> NaturalNumbers()
+        {
+            yield return 0;
+            long n = 1;
+            while (true)
+            {
+                yield return n;
+                n++;
+            }
+        }
+
+        static IEnumerable<long> EvenMembersOf(IEnumerable<long> set)
+        {
+            return set.Where(n => n % 2 == 0);
+        }
+
+        static IEnumerable<long> SquaresOf(IEnumerable<long> set)
+        {
+            return set.Select(n => n * n);
+        }
+
+    }
+}
