c3f0db292674902b214b9e1489f28766e7426af9
Richard Pawson <rpawson@nakedobjects.net>
2017-11-14 15:38:33 +0000

    ... missed file

--- SortingTestbed/SortAlgorithms.cs
+++ SortingTestbed/SortAlgorithms.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortingTestbed
 {
@@ -11,14 +9,64 @@ namespace SortingTestbed
 
         public static void BubbleSort(int[] values)
         {
-            //throw new NotImplementedException();
+            int n = values.Length;
+            //A total of n passes (where n is length of array)
+            //each passing getting one shorter
+            for (int i = n; i > 0; i--)
+            {
+                //Within a pass, compare pairs from the start of the array to the end of the unsorted section
+                for (int j = 0; j < i-1; j++)
+                {
+                    if (values[j] > values[j + 1]) //then swap them
+                    {
+                        var temp = values[j + 1];
+                        values[j + 1] = values[j];
+                        values[j] = temp;
+                    }
+                }
+            }
         }
 
-        public static void MergeSort(int[] values)
+        public static void MergeSort(int[] mergeList)
         {
-            throw new NotImplementedException();
+          if (mergeList.Length > 1)
+            {
+                int mid = mergeList.Length / 2; //Rounds down
+                var leftHalf = mergeList.Take(mid).ToArray();
+                var rightHalf = mergeList.Skip(mid).ToArray();
+                MergeSort(leftHalf);
+                MergeSort(rightHalf);
+                int i = 0;
+                int j = 0;
+                int k = 0;
+                while (i < leftHalf.Length && j < rightHalf.Length)
+                {
+                    if (leftHalf[i] < rightHalf[j])
+                    {
+                        mergeList[k] = leftHalf[i];
+                        i += 1;
+                    } else
+                    {
+                        mergeList[k] = rightHalf[j];
+                        j += 1;
+                    }
+                    k += 1;
+                }
+                //Check if left half has elements not merged
+                while (i < leftHalf.Length)
+                {
+                    mergeList[k] = leftHalf[i];
+                    i += 1;
+                    k += 1;
+                }
+                //Check if right half has elements not merged
+                while (j < rightHalf.Length)
+                {
+                    mergeList[k] = rightHalf[j];
+                    j += 1;
+                    k += 1;
+                }
+            }
         }
-
-        
     }
 }
