ff55fb1dc6722290dba038c1f0fb9fee5836ddb4
Richard Pawson <rpawson@nakedobjects.net>
2017-11-15 10:11:20 +0000

    Functional programming: make Merge sort use Ternary operatos

--- FunctionalProgramming/FunctionalSort.CSharp/FunctionalLibrary/MergeSort.cs
+++ FunctionalProgramming/FunctionalSort.CSharp/FunctionalLibrary/MergeSort.cs
@@ -6,101 +6,62 @@ namespace FunctionalLibrary
     {
         public static FList<T> Sort<T>(FList<T> list, Func<T, T, bool> greaterThan)
         {
-            if (list.Count() < 2)
-            {
-                return list;
-            }
-            else
-            {
-                var half = list.Count() / 2;
-                return Merge(Sort(list.Skip(half), greaterThan), Sort(list.Take(half), greaterThan), greaterThan);
-            }
+            return list.Count() < 2 ?
+                  list :
+                  Merge(Sort(list.Skip(HalfLength(list)), greaterThan), Sort(list.Take(HalfLength(list)), greaterThan), greaterThan);
         }
 
         public static FList<T> Merge<T>(FList<T> a, FList<T> b, Func<T, T, bool> greaterThan)
         {
-            if (a.IsEmpty)
-            {
-                return b;
-            }
-            else if (b.IsEmpty)
-            {
-                return a;
-            }
-            else if (greaterThan(a.Head, b.Head))
-            {
-                return FList.Cons(a.Head, Merge(a.Tail, b, greaterThan));
-            }
-            else
-            {
-                return FList.Cons(b.Head, Merge(a, b.Tail, greaterThan));
-            }
+            return a.IsEmpty ?
+                b :
+                b.IsEmpty ?
+                    a :
+                    greaterThan(a.Head, b.Head) ?
+                        FList.Cons(a.Head, Merge(a.Tail, b, greaterThan)):
+                        FList.Cons(b.Head, Merge(a, b.Tail, greaterThan));
         }
 
         public static FList<string> Sort(FList<string> list, Func<string, string, bool> greaterThan)
         {
-            if (list.Count() < 2)
-            {
-                return list;
-            }
-            else
-            {
-                var half = list.Count() / 2;
-                return Merge(Sort(list.Skip(half), greaterThan), Sort(list.Take(half), greaterThan), greaterThan);
-            }
+            return list.Count() < 2 ?
+                list:
+                Merge(Sort(list.Skip(HalfLength(list)), greaterThan), Sort(list.Take(HalfLength(list)), greaterThan), greaterThan);
         }
 
         public static FList<string> Merge(FList<string> a, FList<string> b, Func<string, string, bool> greaterThan)
         {
-            if (a.IsEmpty)
-            {
-                return b;
-            }
-            else if (b.IsEmpty)
-            {
-                return a;
-            }
-            else if (greaterThan(a.Head, b.Head))
-            {
-                return FList.Cons(a.Head, Merge(a.Tail, b, greaterThan));
-            }
-            else
-            {
-                return FList.Cons(b.Head, Merge(a, b.Tail, greaterThan));
-            }
+            return a.IsEmpty ?
+                b :
+                b.IsEmpty ?
+                    a :
+                    greaterThan(a.Head, b.Head) ?
+                        FList.Cons(a.Head, Merge(a.Tail, b, greaterThan)) :
+                        FList.Cons(b.Head, Merge(a, b.Tail, greaterThan));
         }
 
         public static FList<string> SortAlphabetical(FList<string> list)
         {
-            if (list.Count() < 2)
-            {
-                return list;
-            }
-            else
-            {
-                var half = list.Count() / 2;
-                return MergeAlphabetical(SortAlphabetical(list.Skip(half)), SortAlphabetical(list.Take(half)));
-            }
+            return list.Count() < 2 ?
+                list : 
+                MergeAlphabetical(SortAlphabetical(list.Skip(HalfLength(list))), SortAlphabetical(list.Take(HalfLength(list))));
         }
 
         public static FList<string> MergeAlphabetical(FList<string> a, FList<string> b)
         {
-            if (a.IsEmpty)
-            {
-                return b;
-            }
-            else if (b.IsEmpty)
-            {
-                return a;
-            }
-            else if (string.Compare(a.Head, b.Head) < 0)
-            {
-                return FList.Cons(a.Head, MergeAlphabetical(a.Tail, b));
-            }
-            else
-            {
-                return FList.Cons(b.Head, MergeAlphabetical(a, b.Tail));
-            }
+            return a.IsEmpty?
+                b:
+                b.IsEmpty ?
+                    a:
+                    string.Compare(a.Head, b.Head) < 0 ?
+                        FList.Cons(a.Head, MergeAlphabetical(a.Tail, b)):
+                        FList.Cons(b.Head, MergeAlphabetical(a, b.Tail));
+        }
+
+        //returns half the length of the list, rounded down
+        private static int HalfLength<T>(FList<T> list)
+        {
+            return list.Count() / 2;
         }
     }
 }
