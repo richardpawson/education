1c14c09af22e3241d8b1f9b1bdc7f2fd1cc715ad
Richard Pawson <rpawson@nakedobjects.net>
2018-01-28 18:34:45 +0000

    OOPDraw Starting Position -  changed

--- OOPDraw/MyDrawing.cs
+++ OOPDraw/MyDrawing.cs
@@ -10,17 +10,6 @@ namespace OOPDraw
     {
         public static void Draw()
         {
-            var walls = new Square(0, 0, 100, Color.Blue);
-            var circ = new Circle(0, 0, 50, Color.Red);
-            walls.Draw();
-            Program.RefreshDisplay();
-            Program.WaitASecond();
-            Program.ClearDisplay();
-            walls.PositionX = 50;
-            circ.Draw();
-            walls.Draw();
-
         }
-
     }
 }
