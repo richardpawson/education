db69bddef93f111215694c0ae150551ee391889e
Richard Pawson <rpawson@nakedobjects.net>
2018-03-02 10:31:45 +0000

    Added Academy project (to demo REST)

---
+++ Academy/Academy.SeedData/ExampleDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Academy.DataBase;
+using Academy.Model;
+
+namespace Academy.SeedData
+{
+    public class ExampleDbInitializer : DropCreateDatabaseIfModelChanges<ExampleDbContext>
+    {
+        private ExampleDbContext Context;
+        protected override void Seed(ExampleDbContext context)
+        {
+            this.Context = context;
+            AddNewStudent("Alie Algol");
+            AddNewStudent("Forrest Fortran");
+            AddNewStudent("James Java");
+        }
+
+        private void AddNewStudent(string name)
+        {
+            var st = new Student() { FullName = name };
+            Context.Students.Add(st);
+        }
+    }
+}
