c0472533471d1b4761d262087553ee057fd1c63c
Richard Pawson <rpawson@nakedobjects.net>
2018-10-16 20:22:19 +0100

    Roman Numerals
    Removal of old/redundant files.
    First sketch of moderate approach (between brute force and elegant/slow)

--- Roman Numerals/Roman Numerals brute-force TM - Transition Function Generator/Program.cs
+++ Roman Numerals/Roman Numerals brute-force TM - Transition Function Generator/Program.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleApp1
 {
+    static class Constants
+    {
+        public const char blank = underscore;
+        public const char underscore = '_';
+        public const char space = ' ';
+        public const string startState = "S0";
+        public const string haltState = "S*";
+    }
     class Program
     {
-        const char blank = ' ';
-        const string startState = "S0";
-        const string haltState = "S*";
+
 
         static void Main(string[] args)
         {
-            foreach (var rule in RulesForRange(5))
+            var rules = RulesForRange(2100);
+
+            string path = @"C:\Users\rpaws\Desktop\RomanNumerals TM (brute force) for online simulator.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
             {
-                Console.Write(rule.Format1());
+                sw.WriteLine("init:" + Constants.startState);
+                sw.WriteLine("accept:" + Constants.haltState);
+                sw.WriteLine();
+                foreach (var rule in rules)
+                {
+                    sw.WriteLine(rule.Format2Selector());
+                    sw.WriteLine(rule.Format2Action());
+                    sw.WriteLine();
+                }
             }
-            Console.ReadKey();
+
+             path = @"C:\Users\rpaws\Desktop\RomanNumerals TM (brute force) for AQA AL emulator.txt";
+
+            // Create a file to write to.
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                foreach (var rule in rules)
+                {
+                    sw.WriteLine(rule.Format1());
+                }
+            }
         }
 
         static List<Rule> RulesForRange(int max)
@@ -26,19 +55,19 @@ namespace ConsoleApp1
                 string newState = State(i);
                 char read = RightMostDigit(i);
                 var previousState = "S" + TruncateLastDigit(i);
-                rules.Add(new Rule(previousState, read, newState, blank, Direction.Right));
+                rules.Add(new Rule(previousState, read, newState, Constants.blank, Direction.Right));
                 var result = SubtractLargestPossibleSymbol(i);
                 char write = result.Item1;
                 int remainingValue = result.Item2;
-                string nextState = remainingValue > 0 ? State(remainingValue) : haltState;
-                rules.Add(new Rule(newState, blank, nextState, write, Direction.Right));
+                string nextState = remainingValue > 0 ? State(remainingValue) : Constants.haltState;
+                rules.Add(new Rule(newState, Constants.blank, nextState, write, Direction.Right));
             }
             return rules;
         }
 
         static string State(int n)
         {
-            return n > 0 ? "S" + n : startState;
+            return n > 0 ? "S" + n : Constants.startState;
         }
 
         //Returns the symbol and the REMAINING value
@@ -96,15 +125,21 @@ namespace ConsoleApp1
         //This is the format for use with RP's Assembly Language Turing Machine
         public string Format1()
         {
+            var ReadChar = Read == Constants.underscore ? Constants.blank : Read;
+            var WriteChar = Read == Constants.underscore ? Constants.blank : Write;
             var dir = Direction == Direction.Right ? 'R' : 'L';
-            return "d(" + PreviousState + "," + Read + ")=(" + NewState + "," + Write + "," + dir + ")";
+            return "d(" + PreviousState + "," + ReadChar + ")=(" + NewState + "," + WriteChar + "," + dir + ")";
         }
 
         //Format for use with https://turingmachinesimulator.com/ 
-        public string Format2()
+        public string Format2Selector()
+        {
+            return PreviousState + "," + Read;
+        }
+        public string Format2Action()
         {
             var dir = Direction == Direction.Right ? '>' : '<';
-            return PreviousState + "," + Read + "\n" + NewState + "," + Write + "," + dir+"\n";
+            return NewState + "," + Write + "," + dir;
         }
     }
 
