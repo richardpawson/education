33a3e45ef7328f1da4dc1ca83c50929a57356a66
Richard Pawson <rpawson@nakedobjects.net>
2018-11-22 17:13:14 +0000

    Tidy up

--- Roman Numerals/RomanNumeralsC#/UsingFunctionalProgramming.cs
+++ Roman Numerals/RomanNumeralsC#/UsingFunctionalProgramming.cs
@@ -5,16 +5,16 @@ namespace CSharpRomanNumerals
 {
     public static class Convertor
     {
-        public static string Roman(int n)
-        {
-            var units = FList.New(U("M", 1000), U("CM", 900), U("D", 500), U("CD", 400), U("C", 100), U("XC", 90), U("L", 50), U("XL", 40), U("X", 10), U("IX", 9), U("V", 5), U("IV", 4), U("I", 1));
-            var usable = FList.Filter(u => u.Item2 <= n,units);
-            return n == 0 ? "" : FList.Head(usable).Item1 + Roman(n - FList.Head(usable).Item2);          
-        }
+public static string Roman(int n)
+{
+    var units = FList.New(U("M", 1000), U("CM", 900), U("D", 500), U("CD", 400), U("C", 100), U("XC", 90), U("L", 50), U("XL", 40), U("X", 10), U("IX", 9), U("V", 5), U("IV", 4), U("I", 1));
+    var usable = FList.Filter(u => u.Item2 <= n,units);
+    return n == 0 ? "" : FList.Head(usable).Item1 + Roman(n - FList.Head(usable).Item2);          
+}
 
-        private static Tuple<string, int> U(string s, int n)
-        {
-            return Tuple.Create(s, n);
-        }
+private static Tuple<string, int> U(string s, int n)
+{
+    return Tuple.Create(s, n);
+}
     }
 }
