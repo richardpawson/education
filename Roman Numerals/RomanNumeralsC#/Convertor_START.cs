612e71bedf869911b51a84d9f6a22c0b24ede9d1
Richard Pawson <rpawson@nakedobjects.net>
2019-02-18 16:32:42 +0000

    Roman Numerals (Haskell)

--- Roman Numerals/RomanNumeralsC#/Convertor_START.cs
+++ Roman Numerals/RomanNumeralsC#/Convertor_START.cs
@@ -1,13 +1,34 @@
-﻿namespace CSharpRomanNumerals
+﻿using System;
+
+namespace CSharpRomanNumerals
 {
     public static class Convertor
     {
+
+
+
         public static string Roman(int number)
         {
-            throw new NotImplementedException();
+            string result = "";
+            if (number == 5)
+            {
+                result += "V";
+                number -= 5;
+            }
+            if (number == 4)
+            {
+                result += "IV";
+                number -= 4;
+            }
+            for (int i = 0; i <= number; i++)
+            {
+                result += "I";
+                number -= 1;
+            }
+            return result;
         }
 
-        //You can write private functions here and call them from within
-        //AsRomanNumeral
+
+
     }
 }
